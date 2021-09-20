802fa8cef35adef37ba2a72f9c8e1ef0f15b1d8e
p22cobrien <p22cobrien@fairfieldprep.org>
2021-09-20 09:33:04 -0400

    update

--- hw2-2.py
+++ hw2-2.py
@@ -1,17 +1,17 @@
 # Author: CMOB 9/17/2021
 
 free = 1
-basket =  2
+basket = 2
 three = 3
 
 points = str((three * 3) + (basket * 2) + free)
 
-part_1 = str(object ="the plater scored ") 
+part_1 = str(object="the player scored ")
 
-part_2 = str(object =" points in the game")
+part_2 = str(object=" points in the game")
 
 part_3 = part_1 + points
 
-part_4 = part_3 + part_2 
+part_4 = part_3 + part_2
 
 print(part_4)
